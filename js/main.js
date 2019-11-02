bd5ba33f36bd28ad9650aa85eb617e647b0d7c5f
Ubuntu <ubuntu@ip-172-31-86-73.ec2.internal>
2019-11-02 21:42:10 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+alert("Its alive!")
+
+var a = 5;
+var b = 7;
+
+// function print (5*7){
+//     console.log(a*b);
+// }
+
---
+++ js/main.py
@@ -0,0 +1,19 @@
+
+counter = 0
+def riddle_game():
+    
+
+    while True:
+        print("i am tall when i am old and short when I am young")
+        print("type your guess or type 'help one' 'help two' or I give up")
+counter += 1
+answer = input()
+        
+        if answer == "candle":
+            print("you win!")
+          
+            break
+        #     # print("it took you" + str(counter) + "gueses to get the answer")
+        #     # break
+        # else:
+        #     print("you go the wrong answer try again")s
